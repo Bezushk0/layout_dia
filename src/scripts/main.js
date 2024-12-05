59c7199bebb23087fa960409ecaeb46f1164299c
Vladislav Bezushko <bezushkovladis@gmail.com>
2024-12-05 11:06:32 +0200

    make build

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-import { validateForm } from './_form.js';
-import { initSlider } from './_slider.js';
-import { initializeMenu } from './_menu.js';
-
-initSlider();
-validateForm();
-initializeMenu();
-
